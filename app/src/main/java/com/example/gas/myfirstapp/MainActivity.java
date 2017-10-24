f8e699ed80876b53d058ad90a8df043a907665e7
Gaston Bengolea Monzon <gastonbengolea@gmail.com>
2017-10-24 22:44:11 +0100

    Disable button iif weigth text is empty.

--- app/src/main/java/com/example/gas/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/gas/myfirstapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.gas.myfirstapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +21,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         weightText = (EditText) findViewById(R.id.weightText);
+        Button enterButton = (Button) findViewById(R.id.enterButton);
+        disableButtonIffInputEmpty(weightText, enterButton);
+    }
+
+    private void disableButtonIffInputEmpty(EditText weightText, final Button button) {
+        // Initially disable button.
+        button.setEnabled(false);
+
+        weightText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                button.setEnabled(s.length() > 0);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // Do nothing.
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Do nothing.
+            }
+        });
     }
 
     /** Called when the user taps the Send button */
