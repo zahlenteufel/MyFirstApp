1b0804ee38cfad0b671b3d7fed8e4e3357a74fd7
Gaston Bengolea Monzon <gastonbengolea@gmail.com>
2017-10-24 23:06:43 +0100

    Make done button in keyboard enter weight.

--- app/src/main/java/com/example/gas/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/gas/myfirstapp/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         weightText = (EditText) findViewById(R.id.weightText);
         Button enterButton = (Button) findViewById(R.id.enterButton);
         disableButtonIffInputEmpty(weightText, enterButton);
+        makeDoneButtonOnKeyboardEnterWeight();
     }
 
     private void disableButtonIffInputEmpty(EditText weightText, final Button button) {
@@ -48,8 +52,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void makeDoneButtonOnKeyboardEnterWeight() {
+        weightText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    if (weightText.getText().length() > 0) {
+                        sendMessage(v);
+                    }
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+    }
+
     /**
-     * Called when the user taps the Send button.
+     * Called when the user taps the Enter button or Done button in the keyboard while writing
+     * the weight.
      */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, Graph.class);
