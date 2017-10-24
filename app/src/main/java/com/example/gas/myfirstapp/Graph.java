8b8be4dc94695f1314a7360b2444ae61ac20911f
Gaston Bengolea Monzon <gastonbengolea@gmail.com>
2017-10-25 00:00:52 +0100

    Populate weights table.
    We are using the datapoints used to plot, we should populate with the dates.
    
    Also the table can use some formatting.

--- app/src/main/java/com/example/gas/myfirstapp/Graph.java
+++ app/src/main/java/com/example/gas/myfirstapp/Graph.java
@@ -7,7 +7,10 @@ import android.icu.text.DateFormat;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
@@ -28,8 +31,9 @@ import java.util.Scanner;
 public class Graph extends AppCompatActivity {
 
     public static String RECORD_FILE = "weights.txt";
-
     private static DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd");
+
+    private TableLayout weightsTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,16 +55,39 @@ public class Graph extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        TextView textView = (EditText) findViewById(R.id.multilineTextView);
+        weightsTable = (TableLayout) findViewById(R.id.weightsTable);
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
         try {
             DataPoint[] dataPoints = readDataPoints();
             LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
             graph.addSeries(series);
-            textView.setText(series.toString());
+            populateWeightsTable(dataPoints);
         } catch (ParseException e) {
-            // do nothing yet.. show an error?
+            // TODO: Show a popup saying the weight records are invalid and reset them.
+        }
+    }
+
+    private void populateWeightsTable(DataPoint[] dataPoints) {
+        for (DataPoint dataPoint : dataPoints) {
+            // Create a new row to be added.
+            TableRow tr = new TableRow(this);
+            tr.setLayoutParams(
+                    new TableRow.LayoutParams(
+                            TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+
+            TextView date = new TextView(this);
+            date.setText(String.valueOf(dataPoint.getX()));
+            tr.addView(date);
+
+            TextView weight = new TextView(this);
+            weight.setText(String.valueOf(dataPoint.getY()));
+            tr.addView(weight);
+
+            weightsTable.addView(tr,
+                    new TableLayout.LayoutParams(
+                            TableLayout.LayoutParams.FILL_PARENT,
+                            TableLayout.LayoutParams.WRAP_CONTENT));
         }
     }
 
